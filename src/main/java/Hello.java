320ad0ba4d557351185bad2a11353525dbbdf8da
lijas <lijason991@gmail.com>
2025-09-28 16:53:01 -0400

    first commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,21 @@
+public class Hello {
+    public static void main(String[] args) {
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+
+
+    }
+    public static void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+    public static void sayGoodMorning() {
+        System.out.println("Good morning!");
+    }
+
+
+}
